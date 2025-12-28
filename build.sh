5887771731f3c63d4c4b177ebc9d165477a3cb68
achekuri19 <akshay.chekuri@gmail.com>
2025-12-28 02:07:09 +0000

    cuda detect native arch

--- build.sh
+++ build.sh
@@ -4,11 +4,12 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 usage()
 {
-    echo "usage: ./build.sh --arch [x86_64|aarch64] [--with-cuda]"
+    echo "usage: ./build.sh --arch [x86_64|aarch64] [--with-cuda] [--clean]"
 }
 
 TARGET_ARCH=""
 WITH_CUDA="OFF"
+BUILD_CLEAN=0
 
 while [ "$1" != "" ]; do
     case $1 in
@@ -16,6 +17,8 @@ while [ "$1" != "" ]; do
                                 TARGET_ARCH=$1
                                 ;;
         --with-cuda )           WITH_CUDA="ON"
+                                ;;
+        --clean )               BUILD_CLEAN=1
                                 ;;
         -h | --help )           usage
                                 exit
@@ -28,6 +31,9 @@ done
 
 # TODO: add CLI option for build type
 BUILD_TYPE="Debug"
+
+# TODO: add CLI option for CUDA architectures
+# CUDA_ARCHS="native"
 
 # Validate input
 if [ -z "$TARGET_ARCH" ]; then
@@ -47,8 +53,14 @@ fi
 
 # Start main build commands
 mkdir -p "$BUILD_DIR"
+
+if [ "$BUILD_CLEAN" -eq 1 ]; then
+    # remove everything from build directory
+    rm -rf "$BUILD_DIR"/*
+fi
+
 cmake -S "$SCRIPT_DIR" -B "$BUILD_DIR" -DCMAKE_TOOLCHAIN_FILE="$TOOLCHAIN_FILE" -DCMAKE_BUILD_TYPE="$BUILD_TYPE" -DWITH_CUDA="$WITH_CUDA"
-cmake --build "$BUILD_DIR"
+cmake --build "${BUILD_DIR}"
 
 echo "Build completed at: $BUILD_DIR for $TARGET_ARCH with CUDA $WITH_CUDA"
 
